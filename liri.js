4520ea7f37a4607e235239d7236f379078b61a16
Darlene1985 <Darlene_Leslie@ymail.com>
2018-10-31 14:32:51 -0700

    3rd Submit Homework

--- liri.js
+++ liri.js
@@ -8,6 +8,8 @@ const fs = require("fs");
 var request = require("request");
 //npm spotify package
 const Spotify = require('node-spotify-api');
+//npm moment package
+const moment = require('moment')
 //pulls in API key information for Spotify
 const spotify = new Spotify(keys.spotify);
 
@@ -28,7 +30,7 @@ switch (action) {
     bandsInTown(input);
     //goes to the next action
     break;
-   //action 
+  //action 
   case 'spotify-this-song':
     //prints what is being searched
     console.log("Information for the song ", input);
@@ -40,6 +42,8 @@ switch (action) {
   case 'movie-this':
     //prints what is being searched
     console.log("You want movie info on ", input);
+    //call getOmbd function to run
+    getOmbd(input);
     //goes to the next action
     break;
   //action
@@ -61,12 +65,12 @@ function bandsInTown(artist) {
     if (!error && response.statusCode === 200) {
       //created variable to turn string into object
       var info = JSON.parse(body)
-      // var datetime = moment().format('MMMM Do YYYY),
+      var datetime = moment(info[0].datetime).format('MMMM DD, YYYY');
       //displays information found for the concert
       console.log("=====================================================")
-      console.log("Venue: ",info[0].venue.name)
-      console.log("Location: ",info[0].venue.city)
-      console.log("Date: ",info[0].datetime);
+      console.log("Venue: ", info[0].venue.name)
+      console.log("Location: ", info[0].venue.city)
+      console.log("Date: ", datetime);
     }
   });
 }
@@ -81,10 +85,11 @@ function getSpotify(songName) {
     .then(function (response) {
       //displays information about the song
       console.log("=====================================================")
-      console.log("Artist: ",response.tracks.items[0].artists[0].name);
-      console.log("Song: ",response.tracks.items[0].name);
-      // console.log(response.tracks.items[0].album_type);
-      console.log("Link: ",response.tracks.items[0].external_urls.spotify);
+
+      console.log("Artist: ", response.tracks.items[0].artists[0].name);
+      console.log("Song: ", response.tracks.items[0].name);
+      console.log("Album: ", response.tracks.items[0].album.name);
+      console.log("Link: ", response.tracks.items[0].external_urls.spotify);
     })
     //alerts error
     .catch(function (err) {
@@ -94,24 +99,82 @@ function getSpotify(songName) {
 
 
 //create a function that will connect with OMbd API
-//require npm package
-var request = require("request");
-//created variable for user input of movie title
-const movieTitle = input;
-//ajax request from OMdb
-request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-  //error alert 
-  if (!error && response.statusCode === 200) {
-    //created variable converts string into object
-    const movie = JSON.parse(body)
-    //displays information about the movie
-    console.log("=====================================================")
-    console.log("Year: ",movie.Year);
-    console.log("Rated: ",movie.Rated);
-    console.log("Rating: ",movie.Ratings[1]);
-    console.log("Country: ",movie.Country);
-    console.log("Language: ",movie.Language);
-    console.log("Plot: ",movie.Plot);
-    console.log("Cast: ",movie.Actors);
+function getOmbd(movieTitle) {
+  //require npm package
+  var request = require("request");
+  //created variable for user input of movie title
+  var movieTitle = input;
+  //ajax request from OMdb
+  request("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    //error alert 
+    if (!error && response.statusCode === 200) {
+      //created variable converts string into object
+      const movie = JSON.parse(body)
+      //displays information about the movie
+      console.log("=====================================================")
+      console.log("Year: ", movie.Year);
+      console.log("Rated: ", movie.Rated);
+      console.log("Rating: ", movie.Ratings[1]);
+      console.log("Country: ", movie.Country);
+      console.log("Language: ", movie.Language);
+      console.log("Plot: ", movie.Plot);
+      console.log("Cast: ", movie.Actors);
+    }
+  })
+};
+
+
+
+// This block of code will read from the "random.txt" file.
+// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+// The code will store the contents of the reading inside the variable "data"
+fs.readFile("random.txt", "utf8", function (error, data) {
+  // If the code experiences any errors it will log the error to the console.
+  if (error) {
+    return console.log(error);
   }
+
+  action = data.split(",")[0];
+  input = data.split(",")[1]
+
+  console.log(action)
+
+  switch (action) {
+    //action
+    case 'concert-this':
+      //prints what is being searched
+      console.log("Concert for ", input);
+      //calls bandsInTown function to run
+      bandsInTown(input);
+      //goes to the next action
+      break;
+    //action 
+    case 'spotify-this-song':
+      //prints what is being searched
+      console.log("Information for the song ", input);
+      //calls getSpotify function to run
+      getSpotify(input);
+      //goes to the next action
+      break;
+    //action  
+    case 'movie-this':
+      //prints what is being searched
+      console.log("You want movie info on ", input);
+      //call getOmbd function to run
+      getOmbd(input);
+      //goes to the next action
+      break;
+    //action
+    case 'do-what-it-says':
+      console.log("Do what it says");
+      //goes to the next action
+      break;
+    //action
+    default:
+      //prints that we need to enter information another way/correct way
+      console.log("No idea what you are asking for...");
+  }
+
+  // We will then print the contents of data
+  console.log(data);
 });
